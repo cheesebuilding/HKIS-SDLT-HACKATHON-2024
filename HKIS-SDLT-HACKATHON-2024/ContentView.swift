ae35fd0e302ce8a424cc68eb51ba02d7f52d729c
cheesebuilding <micahchen004@gmail.com>
2024-03-03 10:11:29 +0800

    Initial Commit

---
+++ HKIS-SDLT-HACKATHON-2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HKIS-SDLT-HACKATHON-2024
+//
+//  Created by Micah Chen on 3/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HKIS-SDLT-HACKATHON-2024/HKIS_SDLT_HACKATHON_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  HKIS_SDLT_HACKATHON_2024App.swift
+//  HKIS-SDLT-HACKATHON-2024
+//
+//  Created by Micah Chen on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HKIS_SDLT_HACKATHON_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
