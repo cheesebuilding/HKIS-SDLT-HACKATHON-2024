6c7fd7862f4a532c21b7b4a2fed26cadcac63299
cheesebuilding <micahchen004@gmail.com>
2024-03-17 15:15:15 +0800

    Making final touches and polishing up code(there is now one single sheet instead of 2, as with multiple sheets the last sheet replaces the sheet before, which led to errors in which i could not load into the PostHistoryView or the CreatePostView). In addition, when tapping the claim button, the post is remvoed from the box view, I am currently working on displaying the claimed post data to the PostHistoryView.

---
+++ HKIS-SDLT-HACKATHON-2024/PostHistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  PostHistoryView.swift
+//  HKIS-SDLT-HACKATHON-2024
+//
+//  Created by Micah Chen on 3/16/24.
+//
+
+import SwiftUI
+
+struct PostHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostHistoryView()
+}
