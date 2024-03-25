52a23e2548c8a07bfb886a340c6cbed9abb13f1c
cheesebuilding <micahchen004@gmail.com>
2024-03-25 22:51:31 +0800

    Able to sort and search for items: categories in which items are sorted in

---
+++ HKIS-SDLT-HACKATHON-2024/AsyncImage.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImage.swift
+//  HKIS-SDLT-HACKATHON-2024
+//
+//  Created by Micah Chen on 3/20/24.
+//
+
+import SwiftUI
+
+struct AsyncImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImage()
+}
---
+++ HKIS-SDLT-HACKATHON-2024/Supabase.swift
@@ -0,0 +1,18 @@
+//
+//  Supabase.swift
+//  HKIS-SDLT-HACKATHON-2024
+//
+//  Created by Micah Chen on 3/20/24.
+//
+
+import SwiftUI
+
+struct Supabase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Supabase()
+}
